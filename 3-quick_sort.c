5edf6146f7e9525ae4c8cb9cc2993995b9f5bdaa
britfitzm <brittanyfitzm99@gmail.com>
2023-09-23 07:52:32 -0700

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,10 +6,11 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - swap two integers in an array
+ * @a: first integer to swap
+ * @b: second integer to swap
  */
+
 void swap_ints(int *a, int *b)
 {
 	int tmp;
@@ -20,12 +21,12 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * lomuto_partition - Order a subset of an array of integers according to
- *                    the lomuto partition scheme (last element as pivot).
- * @array: The array of integers.
- * @size: The size of the array.
- * @left: The starting index of the subset to order
- * @right: The ending index of the subset to order
+ * lomuto_partition - order a subset of an array of integers according to
+ *	the lomuto partition scheme (last element as pivot)
+ * @array: array of integers
+ * @size: size of array
+ * @left: starting index of the subset to order
+ * @right: ending index of the subset to order
  * Return: final partition index
  */
 
